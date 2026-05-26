2df29e7519cb7ab8f9e1ad30ebf16f6a54f7a274
A Student <student@nowhere.edu>
2026-05-26 01:05:36 +0000

    May 25, 2026, 6:05 PM

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "postNetFunctions.h"
+
+TEST_CASE("encodeDight")
+{
+  CHECK(encodeDigit('0') == "11000");
+  CHECK(encodeDigit('1') == "00011");
+  CHECK(encodeDigit('5') == "01010");
+  CHECK(encodeDigit('8') == "10010");
+}
